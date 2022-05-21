1b7d1ecfbf0ad16835d88d5bc3fb1fa15431225a
Alkhaer <hadi.alkhaer19@gmail.com>
2022-05-21 03:25:07 +0200

    glis

--- include/utils/reponse_game.h
+++ include/utils/reponse_game.h
@@ -7,7 +7,7 @@ int sendSize(int socketclient, struct list_game *_games);
 int sendList(int socketclient, struct list_game *_games);
 int sendUnRegOk(int socketclient, u_int8_t id_partie);
 int sendRegOk(int socketclient, u_int8_t id_partie);
-int sendGlist(struct player *player, struct list_game *list);
+int sendGlist(int socketclient, struct game *current_game);
 int sendWelcome(struct game *game, struct participant *participant, struct list_game *list_game);
 int sendPosit(int client_socket, struct game *game , struct participant *participant);
 
